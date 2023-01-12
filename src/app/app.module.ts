c03f7dd5e8590d0913dc65201256f3113ceec177
Saba Mchedlishvili <74786710+sabam23@users.noreply.github.com>
2023-01-13 02:53:31 +0400

    add dashboard-container & fix nav bar issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './features/nav-bar/nav-bar.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { MobileMenuComponent } from './features/nav-bar/mobile-menu/mobile-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    DashboardComponent,
+    MobileMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
